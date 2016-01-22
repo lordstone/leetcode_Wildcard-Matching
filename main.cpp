c1efa71ccd56e425f28c3c3a74b3f41604d94344
Chengxi (Chen) Shi <chengxis@usc.edu>
2016-01-21 22:47:22 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,49 @@
 class Solution {
 public:
     //DFS
+    class Solution {
+public:
+    //DFS
     bool isMatch(string s, string p) {
         if(s.length() == 0 && p.length() == 0) return true;
         if(p.length() == 0) return false;
-        if(s.length() == 0) return false;
+        //if(s.length() == 0) return false;
         int s_ptr = 0, p_ptr = 0;
         //vector<vector<bool>> res (s.length(), vector<int> (p.length(), false));
-        stack<pair<int, int>> stkStars;
-        while(p_ptr < p.length() || s_ptr < s.length()){
+        int preP = -1, preS = -1;
+        while(s_ptr < s.length()){
             //if(s_ptr == s.length() - 1 && p_ptr < p.length() - 1) return false;
-            if(s_ptr == s.length() - 1 && p_ptr == p.length() - 1) return true;
-            if(p[p_ptr] == '?'{
-                if(p_ptr == p.length() - 1 && s_ptr == s.length() - 1){
-                    return true;
-                }else if(s_ptr == s.length() - 1 || p_ptr == p.length() - 1){
-                    if(stkStars.size() == 0){
-                        return false;
-                    }else{
-                        stkStars.pop();
-                        p_ptr = stkStars.top().second;
-                        s_ptr = stkStars.top().first;
-                    }
-                }else{
-                    p_ptr ++;
-                    s_ptr ++;
-                }
-            }else if(p[p_ptr] == '*'){
-                if(s_ptr == s.length() - 1) return true;
-                if(p_ptr == p.length() - 1) return true;
-                stkStars.push(pair<int, int> (s_ptr, ++p_ptr));
-            }else{//if other char
-                if(s[s_ptr] == p[p_ptr]){
-                    s_ptr ++; p_ptr ++;
-                }else{//if not match
-                    if(stkStars.size() == 0){//if no * available
-                        return false;
-                    }else{//if there is star
-                        if(p_ptr == p.length() - 1 || s_str == s.length() - 1){
-                            //if already cannot make it on current star at the end
-                            stkStars.pop();
-                            if(stkStars.empty()) return false;
-                            p_ptr = stkStars.top().second;
-                            s_ptr = stkStars.top().first;
-                        }else{//if not till the end, use the next star
-                            p_ptr = stkStars.top().second;
-                            s_ptr = ++ (stkStars.top().first);//increment the s start point
-                        }
-                    }
-                }
+            
+            if(p_ptr < p.length() && isTwoMatch(s[s_ptr],p[p_ptr])){
+                p_ptr ++;
+                s_ptr ++;
+            }else if(p[p_ptr] == '*' && p_ptr < p.length()){
+                //if(p_ptr == p.length()) return true;
+                preP = (p_ptr ++);
+                preS = s_ptr;
+            }else if(preP != -1){//if other char
+                p_ptr = preP;
+                s_ptr = ++ preS;
+                p_ptr ++;
+            }else{
+                return false;
             }//end main if
         }//end while
+        while(p_ptr < p.length()){
+            if(p[p_ptr] == '*'){
+                p_ptr ++;
+            }else{
+                return false;
+            }
+        }
         return true;
-    }//end main func
+    }
+    bool isTwoMatch(char a, char b){
+        return a == b || b == '?';
+    }
+};
+
+
     //memory limit exceeds
     /*
     bool isMatch(string s, string p) {
